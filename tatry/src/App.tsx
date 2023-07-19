336eaf9444851ff72343846a330626453abec745
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-07-19 19:13:03 +0200

    init: start tatry task

---
+++ tatry/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Container from "./Container";
+
+function App() {
+  return (
+    <>
+      <Container />
+    </>
+  );
+}
+
+export default App;
---
+++ tatry/src/Container.tsx
@@ -0,0 +1,6 @@
+
+const Container = () => {
+  return <div></div>;
+};
+
+export default Container;
